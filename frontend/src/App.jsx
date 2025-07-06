f3b0769e64d6e3f38754c8a0f5370235f61e2b74
JonathanNevarez <neva_rez00@hotmail.com>
2025-07-06 16:28:59 -0500

    mindy.2.10

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Inicio from './pages/Inicio';
+import Perfil from './pages/Perfil';
+import EditarPerfil from './pages/EditarPerfil';
+import PerfilUsuario from './pages/PerfilUsuario';
+import ChatPage from './pages/ChatPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import socket from './socket';
+
 const App = () => {
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -11,7 +24,7 @@ const App = () => {
 
     return () => socket.disconnect();
   }, []);
-
+  
   return (
     <Routes>
       <Route path="/" element={<Home />} />
